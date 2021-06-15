98e91681e941a02e6c4199784e7b9b75e6c22302
mchaitra96 <chaitra@grassrootsbpo.in>
2021-06-15 11:30:02 +0530

    change for the port autodailer

--- src/modules/dashboard-360/utils/endpoints.js
+++ src/modules/dashboard-360/utils/endpoints.js
@@ -1,6 +1,6 @@
-export const AUTH = 'http://106.51.86.75:5555/api'
-export const AGENT_SERVICE = 'http://106.51.86.75:5000/api'
-export const AMI = 'http://106.51.86.75:52005/ami'
+export const AUTH = 'http://106.51.86.75:52003/api'
+export const AGENT_SERVICE = 'http://106.51.86.75:52001/api'
+export const AMI = 'http://106.51.86.75:52002/ami'
 
 
 export const CAMPAIGN = 'http://106.51.86.75:62008/api'
