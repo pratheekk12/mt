fe4e8d310a175636f5efdea2d82e110bfd8060fc
mchaitra96 <chaitra@grassrootsbpo.in>
2021-06-15 11:34:34 +0530

    change for api domain

--- src/modules/dashboard-360/utils/endpoints.js
+++ src/modules/dashboard-360/utils/endpoints.js
@@ -1,11 +1,11 @@
-export const AUTH = 'http://106.51.86.75:52003/api'
-export const AGENT_SERVICE = 'http://106.51.86.75:52001/api'
-export const AMI = 'http://106.51.86.75:52002/ami'
+export const AUTH = 'http://moneytap.granalytics.in:52003/api'
+export const AGENT_SERVICE = 'http://moneytap.granalytics.in:52001/api'
+export const AMI = 'http://moneytap.granalytics.in:52002/ami'
 
 
-export const CAMPAIGN = 'http://106.51.86.75:62008/api'
-export const CAMPAIGN_REPORT = 'http://106.51.86.75:33010'
-export const UPLOAD_FILE = 'http://106.51.86.75:62010'
+export const CAMPAIGN = 'http://moneytap.granalytics.in:62008/api'
+export const CAMPAIGN_REPORT = 'http://moneytap.granalytics.in:33010'
+export const UPLOAD_FILE = 'http://moneytap.granalytics.in:62010'
 
 
 
