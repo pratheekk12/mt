4ea5dc72b6b58977b36d17eddfb782b7d8356952
pratheekmurthy <pratheekkmurthy516@gmail.com>
2021-06-17 11:32:57 +0530

    bug resolved

--- src/modules/dashboard-360/utils/endpoints.js
+++ src/modules/dashboard-360/utils/endpoints.js
@@ -5,9 +5,9 @@ export const AMI = 'http://moneytap.granalytics.in:52002/ami'
 
 export const CAMPAIGN = 'http://moneytap.granalytics.in:62008/api'
 export const CAMPAIGN_REPORT = 'http://moneytap.granalytics.in:33010'
-export const UPLOAD_FILE = 'http://192.168.4.44:62010'
+export const UPLOAD_FILE = 'http://192.168.3.36:62010'
 
-export const AGENT_PERFORMANCE = 'http://192.168.4.44:62001/api'
+export const AGENT_PERFORMANCE = 'http://192.168.3.36:62001/api'
 
 
 
